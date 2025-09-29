a9296b34b9f71fbb8c59f28010c2fe985afe50b5
Nguyễn Tiến Dũng <ntd432@James-MacBook-Pro.local>
2025-09-29 16:02:27 +0200

    .

--- examples/hello-world-fibonacci/RunnerConfigPJ.py
+++ examples/hello-world-fibonacci/RunnerConfigPJ.py
@@ -121,15 +121,15 @@ class RunnerConfig:
         """Perform any activity here required for stopping measurements."""
         stdout = self.profiler.terminate()
         # os.kill(self.profiler.pid, signal.SIGINT) # graceful shutdown of powerjoular
-        self.profiler.wait()
-        self.performance_profiler.terminate()
-        self.performance_profiler.wait()
+        # self.profiler.wait()
+        # self.performance_profiler.terminate()
+        # self.performance_profiler.wait()
 
     def stop_run(self, context: RunnerContext) -> None:
         """Perform any activity here required for stopping the run.
         Activities after stopping the run should also be performed here."""
-        self.target.terminate()
-        self.target.wait()
+        # self.target.terminate()
+        # self.target.wait()
         self.timestamp_end = datetime.now()
 
     def populate_run_data(self, context: RunnerContext) -> Optional[Dict[str, Any]]:
