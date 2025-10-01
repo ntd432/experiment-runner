f9b4daef42e450b798560f988e04a69ac60de41f
Nguyễn Tiến Dũng <ntd432@James-MacBook-Pro.local>
2025-10-01 18:57:33 +0200

    Change example code

--- examples/linux-powerjoular-profiling/guideline.py
+++ examples/linux-powerjoular-profiling/guideline.py
@@ -1,28 +1,78 @@
-from typing import List
+# The Computer Language Benchmarks Game
+# http://benchmarksgame.alioth.debian.org/
+#
+# contributed by Antoine Pitrou
+# modified by Dominique Wahli and Daniel Nanz
+# modified by Joerg Baumann
+# Binary tree
 
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import config
+import multiprocessing as mp
 
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        memo = { 0 : 1 }
 
-        def dfs(total):
-            if total in memo:
-                return memo[total]
+def make_tree(d):
+    # change here
+    if d > 0:
+        d -= 1
+        return (make_tree(d), make_tree(d))
+    return (None, None)
 
-            res = 0
-            for num in nums:
-                if total < num:
-                    break
-                res += dfs(total - num)
-            memo[total] = res
-            return res
 
-        return dfs(target)
-    
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.combinationSum4(config.C1_ARG[0], int(config.C1_ARG[1])))
+def check_tree(node):
+
+    (l, r) = node
+    if l is None:
+        return 1
+    else:
+        return 1 + check_tree(l) + check_tree(r)
+
+
+def make_check(itde, make=make_tree, check=check_tree):
+
+    i, d = itde
+    return check(make(d))
+
+
+def get_argchunks(i, d, chunksize=5000):
+
+    assert chunksize % 2 == 0
+    chunk = []
+    for k in range(1, i + 1):
+        chunk.extend([(k, d)])
+        if len(chunk) == chunksize:
+            yield chunk
+            chunk = []
+    if len(chunk) > 0:
+        yield chunk
+
+
+def main(n, min_depth=4):
+
+    max_depth = max(min_depth + 2, n)
+    stretch_depth = max_depth + 1
+    if mp.cpu_count() > 1:
+        pool = mp.Pool()
+        chunkmap = pool.map
+    else:
+        chunkmap = map
+
+    print('stretch tree of depth {0}\t check: {1}'.format(
+          stretch_depth, make_check((0, stretch_depth))))
+
+    long_lived_tree = make_tree(max_depth)
+    # change here
+    mmd = max_depth + min_depth
+    i_val = 2 ** mmd
+    for d in range(min_depth, stretch_depth, 2):
+        i = i_val >> d
+        cs = 0
+        for argchunk in get_argchunks(i,d):
+            cs += sum(chunkmap(make_check, argchunk))
+        print('{0}\t trees of depth {1}\t check: {2}'.format(i, d, cs))
+
+    print('long lived tree of depth {0}\t check: {1}'.format(
+          max_depth, check_tree(long_lived_tree)))
+
+
+if __name__ == '__main__':
+    main(20)
--- examples/linux-powerjoular-profiling/no_guideline.py
+++ examples/linux-powerjoular-profiling/no_guideline.py
@@ -1,26 +1,75 @@
-from typing import List
+# The Computer Language Benchmarks Game
+# http://benchmarksgame.alioth.debian.org/
+#
+# contributed by Antoine Pitrou
+# modified by Dominique Wahli and Daniel Nanz
+# modified by Joerg Baumann
+# Binary tree
 
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import config
+import multiprocessing as mp
 
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        nums.sort()
 
-        def dfs(total):
-            if total == 0:
-                return 1
+def make_tree(d):
 
-            res = 0
-            for i in range(len(nums)):
-                if total < nums[i]:
-                    break
-                res += dfs(total - nums[i])
-            return res
+    if d > 0:
+        return (make_tree(d - 1), make_tree(d - 1))
+    return (None, None)
 
-        return dfs(target)
 
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.combinationSum4(config.C1_ARG[0], int(config.C1_ARG[1])))
+def check_tree(node):
+
+    (l, r) = node
+    if l is None:
+        return 1
+    else:
+        return 1 + check_tree(l) + check_tree(r)
+
+
+def make_check(itde, make=make_tree, check=check_tree):
+
+    i, d = itde
+    return check(make(d))
+
+
+def get_argchunks(i, d, chunksize=5000):
+
+    assert chunksize % 2 == 0
+    chunk = []
+    for k in range(1, i + 1):
+        chunk.extend([(k, d)])
+        if len(chunk) == chunksize:
+            yield chunk
+            chunk = []
+    if len(chunk) > 0:
+        yield chunk
+
+
+def main(n, min_depth=4):
+
+    max_depth = max(min_depth + 2, n)
+    stretch_depth = max_depth + 1
+    if mp.cpu_count() > 1:
+        pool = mp.Pool()
+        chunkmap = pool.map
+    else:
+        chunkmap = map
+
+    print('stretch tree of depth {0}\t check: {1}'.format(
+          stretch_depth, make_check((0, stretch_depth))))
+
+    long_lived_tree = make_tree(max_depth)
+
+    for d in range(min_depth, stretch_depth, 2):
+        i = 2 ** (max_depth + min_depth - d)
+        cs = 0
+        for argchunk in get_argchunks(i,d):
+            cs += sum(chunkmap(make_check, argchunk))
+        print('{0}\t trees of depth {1}\t check: {2}'.format(i, d, cs))
+
+    print('long lived tree of depth {0}\t check: {1}'.format(
+          max_depth, check_tree(long_lived_tree)))
+
+
+if __name__ == '__main__':
+    main(20)
